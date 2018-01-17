45af442cc88d8140fdcddeec7dd42f64ebffa680
Raphael Michel <mail@raphaelmichel.de>
2018-01-17 11:35:17 +0100

    Add test infrastructure

---
+++ libpretixsync/src/test/java/eu/pretix/libpretixsync/test/db/QuestionAnswerValidationTest.java
@@ -0,0 +1,12 @@
+package eu.pretix.libpretixsync.test.db;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class QuestionAnswerValidationTest {
+    @Test
+    public void evaluatesExpression() {
+        assertEquals(3, 3);
+    }
+}
