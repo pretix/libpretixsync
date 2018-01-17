5122bae436f043b5839a980e822208c129638d71
Raphael Michel <mail@raphaelmichel.de>
2018-01-17 11:59:08 +0100

    First validation and test steps

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/db/AbstractQuestion.java
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/db/AbstractQuestion.java
@@ -3,10 +3,18 @@ package eu.pretix.libpretixsync.db;
 import eu.pretix.libpretixsync.check.QuestionType;
 import io.requery.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Entity(cacheable = false)
 public class AbstractQuestion {
+
+    public class ValidationException extends Exception {
+        public ValidationException(String msg) {
+            super(msg);
+        }
+    }
+
     @Generated
     @Key
     public Long id;
@@ -27,4 +35,17 @@ public class AbstractQuestion {
     @ManyToMany
     @JunctionTable
     List<Item> items;
+
+    public String clean_answer(String answer) throws ValidationException {
+        if (type == QuestionType.N) {
+            try {
+                return new BigDecimal(answer.toString()).toPlainString();
+            } catch (NumberFormatException e) {
+                throw new ValidationException("Invalid number supplied");
+            }
+        } else if (type == QuestionType.B) {
+            return (answer.equals("True") || answer.equals("true")) ? "True" : "False";
+        }
+        return answer.toString();
+    }
 }
--- libpretixsync/src/test/java/eu/pretix/libpretixsync/test/db/QuestionAnswerValidationTest.java
+++ libpretixsync/src/test/java/eu/pretix/libpretixsync/test/db/QuestionAnswerValidationTest.java
@@ -1,12 +1,68 @@
 package eu.pretix.libpretixsync.test.db;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import eu.pretix.libpretixsync.check.QuestionType;
+import eu.pretix.libpretixsync.db.AbstractQuestion;
+import eu.pretix.libpretixsync.db.Question;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import javax.xml.bind.ValidationException;
+import java.util.Arrays;
+import java.util.Collection;
 
 
+@RunWith(Parameterized.class)
 public class QuestionAnswerValidationTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {QuestionType.S, "a", "a"},
+                {QuestionType.T, "b", "b"},
+                {QuestionType.S, "", ""},
+                {QuestionType.N, "3", "3"},
+                {QuestionType.N, "2.56", "2.56"},
+                {QuestionType.N, "abc", null},
+                {QuestionType.B, "True", "True"},
+                {QuestionType.B, "true", "True"},
+                {QuestionType.B, "False", "False"},
+                {QuestionType.B, "false", "False"},
+                {QuestionType.B, "0", "False"},
+                {QuestionType.B, "", "False"},
+                // TODO: Date, time, datetime
+        });
+    }
+
+    private QuestionType questionType;
+    private String input;
+    private String expected;
+
+    public QuestionAnswerValidationTest(QuestionType questionType, String input, String expected) {
+        this.questionType = questionType;
+        this.input = input;
+        this.expected = expected;
+    }
+
     @Test
-    public void evaluatesExpression() {
-        assertEquals(3, 3);
+    public void test() {
+        Question q = new Question();
+        q.setType(questionType);
+        if (expected == null) {
+            try {
+                q.clean_answer(input);
+                fail("Expected an ValidationException to be thrown");
+            } catch (Question.ValidationException e) {
+            }
+        } else {
+            try {
+                assertEquals(q.clean_answer(input), expected);
+            } catch (AbstractQuestion.ValidationException e) {
+                fail(e.getMessage());
+            }
+        }
     }
 }
