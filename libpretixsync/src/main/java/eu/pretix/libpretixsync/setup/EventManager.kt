6865cec18737fc274e8146d14c62ee05c986d88d
Raphael Michel <mail@raphaelmichel.de>
2018-12-13 15:13:29 +0100

    Add code for API setup

---
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/setup/EventManager.kt
@@ -0,0 +1,75 @@
+package eu.pretix.libpretixsync.setup
+
+import eu.pretix.libpretixsync.api.PretixApi
+import eu.pretix.libpretixsync.config.ConfigStore
+import eu.pretix.libpretixsync.utils.I18nString
+import io.requery.BlockingEntityStore
+import io.requery.Persistable
+import org.joda.time.DateTime
+import org.joda.time.Hours
+import org.json.JSONObject
+import java.io.IOException
+import java.net.URLEncoder
+
+class EventManager(private val store: BlockingEntityStore<Persistable>, private val api: PretixApi, private val conf: ConfigStore) {
+    fun getAvailableEvents() : List<RemoteEvent> {
+        val eightHoursAgo = URLEncoder.encode((DateTime.now() - Hours.hours(8)).toString())
+
+        val resp_events = api.fetchResource(api.organizerResourceUrl("events") +
+                "?live=true&ends_after=$eightHoursAgo")
+        if (resp_events.response.code() != 200) {
+            throw IOException()
+        }
+        var events = parseEvents(resp_events.data)
+
+        val resp_subevents = api.fetchResource(api.organizerResourceUrl("subevents")
+                + "?active=true&event__live=true&ends_after=$eightHoursAgo")
+        if (resp_subevents.response.code() != 200) {
+            throw IOException()
+        }
+        events += parseEvents(resp_subevents.data, subevents=true)
+
+        return events
+    }
+
+    private fun parseEvents(data: JSONObject, subevents: Boolean = false): List<RemoteEvent> {
+        val events = ArrayList<RemoteEvent>()
+        val results = data.getJSONArray("results")
+        for (i in 0 until results.length()) {
+            val json = results.getJSONObject(i)
+            events.add(RemoteEvent(
+                    json.getJSONObject("name"),
+                    if (subevents) json.getString("event") else json.getString("slug"),
+                    DateTime(json.getString("date_from")),
+                    if (json.isNull("date_to")) null else {
+                        DateTime(json.optString("date_to"))
+                    },
+                    if (subevents) {
+                        json.getLong("id")
+                    } else null))
+        }
+
+
+        if (!data.isNull("next")) {
+            val next = data.getString("next")
+            val resp = api.fetchResource(next)
+            if (resp.response.code() != 200) {
+                throw IOException()
+            }
+            return events + parseEvents(resp.data, true)
+        } else {
+            return events
+        }
+    }
+}
+
+data class RemoteEvent(
+        val name_i18n: JSONObject,
+        val slug: String,
+        val date_from: DateTime,
+        val date_to: DateTime?,
+        val subevent_id: Long?
+) {
+    val name: String
+        get() = I18nString.toString(name_i18n)
+}
---
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/setup/SetupManager.kt
@@ -0,0 +1,62 @@
+package eu.pretix.libpretixsync.setup
+
+import eu.pretix.libpretixsync.api.HttpClientFactory
+import eu.pretix.libpretixsync.utils.flatJsonError
+import okhttp3.MediaType
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import org.json.JSONException
+import org.json.JSONObject
+import sun.rmi.runtime.Log
+import java.io.IOException
+import javax.net.ssl.SSLException
+
+
+open class SetupException(override var message: String?) : Exception(message)
+class SetupServerErrorException(override var message: String?) : SetupException(message)
+class SetupBadRequestException(override var message: String?) : SetupException(message)
+class SetupBadResponseException(override var message: String?) : SetupException(message)
+
+data class SetupResult(val url: String, val api_token: String, val organizer: String, val device_id: Long, val unique_serial: String)
+
+class SetupManager(private val hardware_brand: String, private val hardware_model: String, private val software_brand: String, private val software_version: String, private val http_factory: HttpClientFactory) {
+    fun initialize(url: String, token: String): SetupResult {
+        val client = http_factory.buildClient()
+        val apiBody = JSONObject()
+        apiBody.put("token", token)
+        apiBody.put("hardware_brand", hardware_brand)
+        apiBody.put("hardware_model", hardware_model)
+        apiBody.put("software_brand", software_brand)
+        apiBody.put("software_version", software_version)
+
+        val request = Request.Builder()
+                .url(url + "/api/v1/device/initialize")
+                .post(RequestBody.create(MediaType.parse("application/json"), apiBody.toString()))
+                .build()
+        var response: Response? = null
+        response = client.newCall(request).execute();
+
+        if (response.code() >= 500) {
+            response.close()
+            throw SetupServerErrorException(response.body()?.string())
+        } else if (response.code() == 400) {
+            response.close()
+            try {
+                throw SetupBadRequestException(flatJsonError(JSONObject(response?.body()?.string())))
+            } catch (e: JSONException) {
+                throw SetupBadResponseException(response?.body()?.string())
+            }
+        } else {
+            try {
+                val respo = JSONObject(response?.body()?.string())
+                return SetupResult(
+                        url, respo.getString("api_token"), respo.getString("organizer"), respo.getLong("device_id"), respo.getString("unique_serial")
+                )
+            } catch (e: JSONException) {
+                throw SetupBadResponseException(response?.body()?.string())
+                e.printStackTrace()
+            }
+        }
+    }
+}
