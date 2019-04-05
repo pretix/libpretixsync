3a8971a6fed90244ab872cee2c6d24bd5fa884d1
Raphael Michel <mail@raphaelmichel.de>
2019-04-05 15:22:10 +0200

    Improve EventManager

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/setup/EventManager.kt
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/setup/EventManager.kt
@@ -11,19 +11,19 @@ import org.json.JSONObject
 import java.io.IOException
 import java.net.URLEncoder
 
-class EventManager(private val store: BlockingEntityStore<Persistable>, private val api: PretixApi, private val conf: ConfigStore) {
+class EventManager(private val store: BlockingEntityStore<Persistable>, private val api: PretixApi, private val conf: ConfigStore, private val require_live: Boolean) {
     fun getAvailableEvents() : List<RemoteEvent> {
         val eightHoursAgo = URLEncoder.encode((DateTime.now() - Hours.hours(8)).toString())
 
         val resp_events = api.fetchResource(api.organizerResourceUrl("events") +
-                "?live=true&ends_after=$eightHoursAgo")
+                "?ends_after=$eightHoursAgo" + if (require_live) "&live=true" else "")
         if (resp_events.response.code() != 200) {
             throw IOException()
         }
         var events = parseEvents(resp_events.data)
 
         val resp_subevents = api.fetchResource(api.organizerResourceUrl("subevents")
-                + "?active=true&event__live=true&ends_after=$eightHoursAgo")
+                + "?ends_after=$eightHoursAgo" + if (require_live) "active=true&event__live=true" else "")
         if (resp_subevents.response.code() != 200) {
             throw IOException()
         }
@@ -36,19 +36,43 @@ class EventManager(private val store: BlockingEntityStore<Persistable>, private 
 
     private fun parseEvents(data: JSONObject, subevents: Boolean = false): List<RemoteEvent> {
         val events = ArrayList<RemoteEvent>()
+
+        val eventMap = HashMap<String, PretixApi.ApiResponse>()
+
         val results = data.getJSONArray("results")
         for (i in 0 until results.length()) {
             val json = results.getJSONObject(i)
             events.add(RemoteEvent(
-                    json.getJSONObject("name"),
-                    if (subevents) json.getString("event") else json.getString("slug"),
-                    DateTime(json.getString("date_from")),
-                    if (json.isNull("date_to")) null else {
+                    name_i18n=json.getJSONObject("name"),
+                    slug=if (subevents) json.getString("event") else json.getString("slug"),
+                    date_from=DateTime(json.getString("date_from")),
+                    date_to=if (json.isNull("date_to")) null else {
                         DateTime(json.optString("date_to"))
                     },
-                    if (subevents) {
+                    subevent_id=if (subevents) {
                         json.getLong("id")
-                    } else null))
+                    } else null,
+                    live=if (subevents) {
+                        val eventSlug = json.getString("event")
+                        var event = eventMap[json.getString("event")]
+                        if (event == null) {
+                            api.eventSlug = json.getString("event")
+                            try {
+                                event = api.fetchResource(api.organizerResourceUrl("events/" + api.eventSlug))
+
+                                if (event.response.code() != 200) {
+                                    throw IOException()
+                                }
+                                eventMap[json.getString("event")] = event
+                            } finally {
+                                api.eventSlug = conf.eventSlug
+                            }
+                        }
+
+                        event!!.data.getBoolean("live") && json.getBoolean("active")
+                    } else {
+                        json.getBoolean("live")
+                    }))
         }
 
 
@@ -70,7 +94,8 @@ data class RemoteEvent(
         val slug: String,
         val date_from: DateTime,
         val date_to: DateTime?,
-        val subevent_id: Long?
+        val subevent_id: Long?,
+        val live: Boolean
 ) {
     val name: String
         get() = I18nString.toString(name_i18n)
