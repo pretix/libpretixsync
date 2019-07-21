ba1126ab565cb6e8a541e31de32a7e5f7e1b5b45
Raphael Michel <mail@raphaelmichel.de>
2019-07-21 15:14:16 +0200

    Add optional sync adapters to sync all (sub)events

---
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/sync/AllEventsSyncAdapter.java
@@ -0,0 +1,70 @@
+package eu.pretix.libpretixsync.sync;
+
+import eu.pretix.libpretixsync.api.PretixApi;
+import eu.pretix.libpretixsync.db.Event;
+import io.requery.BlockingEntityStore;
+import io.requery.Persistable;
+import io.requery.query.Tuple;
+import org.joda.time.format.ISODateTimeFormat;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class AllEventsSyncAdapter extends BaseDownloadSyncAdapter<Event, String> {
+    public AllEventsSyncAdapter(BlockingEntityStore<Persistable> store, FileStorage fileStorage, String eventSlug, PretixApi api, SyncManager.ProgressFeedback feedback) {
+        super(store, fileStorage, eventSlug, api, feedback);
+    }
+
+    @Override
+    Iterator<Tuple> getKnownIDsIterator() {
+        return store.select(Event.SLUG)
+                .get().iterator();
+    }
+
+    @Override
+    public void updateObject(Event obj, JSONObject jsonobj) throws JSONException {
+        obj.setSlug(jsonobj.getString("slug"));
+        obj.setCurrency(jsonobj.getString("currency"));
+        obj.setDate_from(ISODateTimeFormat.dateTimeParser().parseDateTime(jsonobj.getString("date_from")).toDate());
+        if (!jsonobj.isNull("date_to")) {
+            obj.setDate_to(ISODateTimeFormat.dateTimeParser().parseDateTime(jsonobj.getString("date_to")).toDate());
+        }
+        obj.setLive(jsonobj.getBoolean("live"));
+        obj.setHas_subevents(jsonobj.getBoolean("has_subevents"));
+        obj.setJson_data(jsonobj.toString());
+    }
+
+    @Override
+    protected String getUrl() {
+        return api.organizerResourceUrl(getResourceName());
+    }
+
+    @Override
+    String getResourceName() {
+        return "events";
+    }
+
+    @Override
+    String getId(JSONObject obj) throws JSONException {
+        return obj.getString("slug");
+    }
+
+    @Override
+    String getId(Event obj) {
+        return obj.getSlug();
+    }
+
+    @Override
+    Event newEmptyObject() {
+        return new Event();
+    }
+
+    @Override
+    public Iterator<Event> runBatch(List<String> parameterBatch) {
+        return store.select(Event.class)
+                .where(Event.SLUG.in(parameterBatch))
+                .get().iterator();
+    }
+}
---
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/sync/AllSubEventsSyncAdapter.java
@@ -0,0 +1,70 @@
+package eu.pretix.libpretixsync.sync;
+
+import eu.pretix.libpretixsync.api.PretixApi;
+import eu.pretix.libpretixsync.db.Event;
+import eu.pretix.libpretixsync.db.SubEvent;
+import io.requery.BlockingEntityStore;
+import io.requery.Persistable;
+import io.requery.query.Tuple;
+import org.joda.time.format.ISODateTimeFormat;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class AllSubEventsSyncAdapter extends BaseDownloadSyncAdapter<SubEvent, Long> {
+    public AllSubEventsSyncAdapter(BlockingEntityStore<Persistable> store, FileStorage fileStorage, String eventSlug, PretixApi api, SyncManager.ProgressFeedback feedback) {
+        super(store, fileStorage, eventSlug, api, feedback);
+    }
+
+    @Override
+    Iterator<Tuple> getKnownIDsIterator() {
+        return store.select(SubEvent.SERVER_ID)
+                .get().iterator();
+    }
+
+    @Override
+    public void updateObject(SubEvent obj, JSONObject jsonobj) throws JSONException {
+        obj.setServer_id(jsonobj.getLong("id"));
+        obj.setEvent_slug(jsonobj.getString("event"));
+        obj.setDate_from(ISODateTimeFormat.dateTimeParser().parseDateTime(jsonobj.getString("date_from")).toDate());
+        if (!jsonobj.isNull("date_to")) {
+            obj.setDate_to(ISODateTimeFormat.dateTimeParser().parseDateTime(jsonobj.getString("date_to")).toDate());
+        }
+        obj.setActive(jsonobj.getBoolean("active"));
+        obj.setJson_data(jsonobj.toString());
+    }
+
+    @Override
+    protected String getUrl() {
+        return api.organizerResourceUrl(getResourceName());
+    }
+
+    @Override
+    String getResourceName() {
+        return "subevents";
+    }
+
+    @Override
+    Long getId(JSONObject obj) throws JSONException {
+        return obj.getLong("id");
+    }
+
+    @Override
+    Long getId(SubEvent obj) {
+        return obj.getId();
+    }
+
+    @Override
+    SubEvent newEmptyObject() {
+        return new SubEvent();
+    }
+
+    @Override
+    public Iterator<SubEvent> runBatch(List<Long> parameterBatch) {
+        return store.select(SubEvent.class)
+                .where(SubEvent.SERVER_ID.in(parameterBatch))
+                .get().iterator();
+    }
+}
