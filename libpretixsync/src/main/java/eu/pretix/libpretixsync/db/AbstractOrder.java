4f7295e7f4c1dee94a2f7b8878b8e04d86fd98d4
Raphael Michel <mail@raphaelmichel.de>
2020-01-23 11:35:28 +0100

    Implement Order.getPendingTotal

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/db/AbstractOrder.java
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/db/AbstractOrder.java
@@ -1,6 +1,8 @@
 package eu.pretix.libpretixsync.db;
 
 import io.requery.*;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -44,6 +46,39 @@ public class AbstractOrder implements RemoteObject {
     public BigDecimal getTotal() {
         try {
             return new BigDecimal(getJSON().getString("total"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public BigDecimal getPendingTotal() {
+        try {
+            JSONObject j = getJSON();
+
+            BigDecimal total = new BigDecimal(j.getString("total"));
+            if (j.getString("status").equals("c")) {
+                total = BigDecimal.ZERO;
+            }
+
+            BigDecimal paymentSum = BigDecimal.ZERO;
+            JSONArray payments = j.getJSONArray("payments");
+            for (int i = 0; i < payments.length(); i++) {
+                JSONObject payment = payments.getJSONObject(i);
+                if (payment.getString("state").matches("^(confirmed|refunded)$")) {
+                    paymentSum = paymentSum.add(new BigDecimal(payment.getString("amount")));
+                }
+            }
+
+            BigDecimal refundSum = BigDecimal.ZERO;
+            JSONArray refunds = j.getJSONArray("refunds");
+            for (int i = 0; i < refunds.length(); i++) {
+                JSONObject refund = refunds.getJSONObject(i);
+                if (refund.getString("state").matches("^(done|transit|created)$")) {
+                    refundSum = refundSum.add(new BigDecimal(refund.getString("amount")));
+                }
+            }
+            return total.subtract(paymentSum).add(refundSum);
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
