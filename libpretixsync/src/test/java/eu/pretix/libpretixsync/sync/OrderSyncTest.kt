51e1f3a02c59e7b274e6010fcb4af9346cb74a0e
Raphael Michel <mail@raphaelmichel.de>
2020-08-21 18:42:52 +0200

    Fix test cases

--- libpretixsync/src/test/java/eu/pretix/libpretixsync/sync/OrderSyncTest.kt
+++ libpretixsync/src/test/java/eu/pretix/libpretixsync/sync/OrderSyncTest.kt
@@ -28,9 +28,9 @@ class OrderSyncTest : BaseDatabaseTest() {
     fun setUpFakes() {
         configStore = FakeConfigStore()
         fakeApi = FakePretixApi()
-        osa = OrderSyncAdapter(dataStore, FakeFileStorage(), "demo", fakeApi, null)
+        osa = OrderSyncAdapter(dataStore, FakeFileStorage(), "demo", true, false, fakeApi, null)
 
-        CheckInListSyncAdapter(dataStore, FakeFileStorage(), "demo", fakeApi, null).standaloneRefreshFromJSON(jsonResource("checkinlists/list1.json"))
+        CheckInListSyncAdapter(dataStore, FakeFileStorage(), "demo", 0L, fakeApi, null).standaloneRefreshFromJSON(jsonResource("checkinlists/list1.json"))
     }
 
     private fun response(data: JSONObject, generated: String): Response {
