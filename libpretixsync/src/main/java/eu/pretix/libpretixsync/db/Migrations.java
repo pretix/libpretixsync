8a5a1347f8ca7c52a997f1e14f8a6f16a20f684a
Raphael Michel <mail@raphaelmichel.de>
2020-09-21 09:49:58 +0200

    Silently fail if migrations are executed twice

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/db/Migrations.java
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/db/Migrations.java
@@ -18,20 +18,39 @@ public class Migrations {
 
     private static void createVersionTable(Connection c, int version) throws SQLException {
         Statement s2 = c.createStatement();
-        s2.execute("CREATE TABLE _version (version NUMERIC);");
+        execIgnore(c, "CREATE TABLE _version (version NUMERIC);", "duplicate column name");
         s2.close();
         Statement s3 = c.createStatement();
-        s3.execute("INSERT INTO _version (version) VALUES (" + version + ");");
+        execIgnore(c, "INSERT INTO _version (version) VALUES (" + version + ");", "duplicate column name");
         s3.close();
     }
 
     private static void updateVersionTable(Connection c, int version) throws SQLException {
         Statement s2 = c.createStatement();
-        s2.execute("DELETE FROM _version;");
+        execIgnore(c, "DELETE FROM _version;", "duplicate column name");
         s2.close();
         Statement s3 = c.createStatement();
-        s3.execute("INSERT INTO _version (version) VALUES (" + version + ");");
+        execIgnore(c, "INSERT INTO _version (version) VALUES (" + version + ");", "duplicate column name");
         s3.close();
+    }
+
+    private static void execIgnore(Connection c, String sql, String ignoreMatch) throws SQLException {
+        Statement s1 = c.createStatement();
+        try {
+            s1.execute(sql);
+        } catch (SQLException e) {
+            if (!e.getMessage().contains(ignoreMatch)) {
+                throw e;
+            }
+        } finally {
+            s1.close();
+        }
+    }
+
+    private static void exec(Connection c, String sql) throws SQLException {
+        Statement s1 = c.createStatement();
+        s1.execute(sql);
+        s1.close();
     }
 
     public static void migrate(DataSource dataSource, boolean dbIsNew) throws SQLException {
@@ -59,183 +78,142 @@ public class Migrations {
 
         if (db_version < 24) {
             create_drop(dataSource);
+            updateVersionTable(c, 24);
         }
         if (db_version < 28) {
             create_notexists(dataSource);
+            updateVersionTable(c, 28);
         }
         if (db_version < 30) {
-            Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE Receipt ADD payment_data TEXT;");
-            s1.close();
+            execIgnore(c, "ALTER TABLE Receipt ADD payment_data TEXT;", "duplicate column name");
             create_notexists(dataSource);
+            updateVersionTable(c, 30);
         }
         if (db_version < 32) {
             create_notexists(dataSource);
+            updateVersionTable(c, 32);
         }
         if (db_version < 33) {
-            Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE ResourceLastModified ADD status TEXT;");
-            s1.close();
+            execIgnore(c, "ALTER TABLE ResourceLastModified ADD status TEXT;", "duplicate column name");
             create_notexists(dataSource);
+            updateVersionTable(c, 33);
         }
         if (db_version < 34) {
-            Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE QueuedCheckin ADD event_slug TEXT;");
-            s1.close();
+            execIgnore(c, "ALTER TABLE QueuedCheckin ADD event_slug TEXT;", "duplicate column name");
+            updateVersionTable(c, 34);
         }
         if (db_version < 35) {
-            Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE QueuedOrder ADD locked NUMBER DEFAULT(0);");
-            s1.close();
+            execIgnore(c, "ALTER TABLE QueuedOrder ADD locked NUMBER DEFAULT(0);", "duplicate column name");
+            updateVersionTable(c, 35);
         }
         if (db_version < 36) {
-            Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE QueuedOrder ADD idempotency_key TEXT;");
-            s1.close();
+            execIgnore(c, "ALTER TABLE QueuedOrder ADD idempotency_key TEXT;", "duplicate column name");
+            updateVersionTable(c, 36);
         }
         if (db_version < 37) {
-            Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE Item ADD picture_filename TEXT;");
-            s1.close();
+            execIgnore(c, "ALTER TABLE Item ADD picture_filename TEXT;", "duplicate column name");
+            updateVersionTable(c, 37);
         }
         if (db_version < 38) {
-            Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE ReceiptLine ADD seat_guid TEXT;");
-            s1.close();
-            Statement s2 = c.createStatement();
-            s2.execute("ALTER TABLE ReceiptLine ADD seat_name TEXT;");
-            s2.close();
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD seat_guid TEXT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD seat_name TEXT;", "duplicate column name");
+            updateVersionTable(c, 38);
         }
         if (db_version < 39) {
-            Statement s4 = c.createStatement();
-            s4.execute("CREATE INDEX orderposition_secret ON orderposition (secret);");
-            s4.close();
+            execIgnore(c, "CREATE INDEX orderposition_secret ON orderposition (secret);", "already exists");
+            updateVersionTable(c, 39);
         }
         if (db_version < 46) {
             Statement s4 = c.createStatement();
-            s4.execute("ALTER TABLE Receipt ADD email_to TEXT;");
+            execIgnore(c, "ALTER TABLE Receipt ADD email_to TEXT;", "duplicate column name");
             s4.close();
+            updateVersionTable(c, 46);
         }
         if (db_version < 47) {
-            Statement s2 = c.createStatement();
-            s2.execute("ALTER TABLE ReceiptLine ADD price_calculated_from_net INT;");
-            s2.close();
-            Statement s3 = c.createStatement();
-            s3.execute("ALTER TABLE ReceiptLine ADD canceled_because_of_receipt INT;");
-            s3.close();
-            Statement s4 = c.createStatement();
-            s4.execute("ALTER TABLE Receipt ADD fiscalisation_data TEXT;");
-            s4.close();
-            Statement s5 = c.createStatement();
-            s5.execute("ALTER TABLE Receipt ADD fiscalisation_text TEXT;");
-            s5.close();
-            Statement s6 = c.createStatement();
-            s6.execute("ALTER TABLE Receipt ADD fiscalisation_qr TEXT;");
-            s6.close();
-            Statement s7 = c.createStatement();
-            s7.execute("ALTER TABLE Receipt ADD started DATE;");
-            s7.close();
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD price_calculated_from_net INT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD canceled_because_of_receipt INT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE Receipt ADD fiscalisation_data TEXT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE Receipt ADD fiscalisation_text TEXT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE Receipt ADD fiscalisation_qr TEXT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE Receipt ADD started DATE;", "duplicate column name");
+            updateVersionTable(c, 47);
         }
         if (db_version < 48) {
             Statement s4 = c.createStatement();
-            s4.execute("ALTER TABLE Closing ADD invoice_settings TEXT;");
+            execIgnore(c, "ALTER TABLE Closing ADD invoice_settings TEXT;", "duplicate column name");
             s4.close();
+            updateVersionTable(c, 48);
         }
         if (db_version < 49) {
             Statement s3 = c.createStatement();
-            s3.execute("ALTER TABLE Item ADD ticket_layout_pretixpos_id INT;");
+            execIgnore(c, "ALTER TABLE Item ADD ticket_layout_pretixpos_id INT;", "duplicate column name");
             s3.close();
+            updateVersionTable(c, 49);
         }
         if (db_version < 50) {
             Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE QueuedCheckin ADD datetime_string TEXT;");
+            execIgnore(c, "ALTER TABLE QueuedCheckin ADD datetime_string TEXT;", "duplicate column name");
             s1.close();
+            updateVersionTable(c, 50);
         }
         if (db_version < 51) {
             Statement s1 = c.createStatement();
-            s1.execute("ALTER TABLE QueuedCheckin ADD type TEXT;");
+            execIgnore(c, "ALTER TABLE QueuedCheckin ADD type TEXT;", "duplicate column name");
             s1.close();
             s1 = c.createStatement();
-            s1.execute("ALTER TABLE CheckIn ADD type TEXT;");
+            execIgnore(c, "ALTER TABLE CheckIn ADD type TEXT;", "duplicate column name");
             s1.close();
+            updateVersionTable(c, 51);
         }
         if (db_version < 52) {
             Statement s2 = c.createStatement();
-            s2.execute("ALTER TABLE ReceiptLine ADD answers TEXT;");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD answers TEXT;", "duplicate column name");
             s2.close();
+            updateVersionTable(c, 52);
         }
         if (db_version < 53) {
-            Statement s2 = c.createStatement();
-            s2.execute("CREATE TABLE Settings (id integer primary key autoincrement, address TEXT, city varchar(255), country varchar(255), json_data TEXT, name varchar(255), slug varchar(255), tax_id varchar(255), vat_id varchar(255), zipcode varchar(255));");
-            s2.close();
+            exec(c, "CREATE TABLE IF NOT EXISTS Settings (id integer primary key autoincrement, address TEXT, city varchar(255), country varchar(255), json_data TEXT, name varchar(255), slug varchar(255), tax_id varchar(255), vat_id varchar(255), zipcode varchar(255));");
+            updateVersionTable(c, 53);
         }
         if (db_version < 54) {
-            Statement s2 = c.createStatement();
-            s2.execute("ALTER TABLE ReceiptLine ADD attendee_name INT;");
-            s2.close();
-            Statement s3 = c.createStatement();
-            s3.execute("ALTER TABLE ReceiptLine ADD attendee_email INT;");
-            s3.close();
-            Statement s4 = c.createStatement();
-            s4.execute("ALTER TABLE ReceiptLine ADD attendee_company TEXT;");
-            s4.close();
-            Statement s5 = c.createStatement();
-            s5.execute("ALTER TABLE ReceiptLine ADD attendee_street TEXT;");
-            s5.close();
-            Statement s6 = c.createStatement();
-            s6.execute("ALTER TABLE ReceiptLine ADD attendee_zipcode TEXT;");
-            s6.close();
-            Statement s7 = c.createStatement();
-            s7.execute("ALTER TABLE ReceiptLine ADD attendee_city DATE;");
-            s7.close();
-            Statement s8 = c.createStatement();
-            s8.execute("ALTER TABLE ReceiptLine ADD attendee_country DATE;");
-            s8.close();
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD attendee_name INT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD attendee_email INT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD attendee_company TEXT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD attendee_street TEXT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD attendee_zipcode TEXT;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD attendee_city DATE;", "duplicate column name");
+            execIgnore(c, "ALTER TABLE ReceiptLine ADD attendee_country DATE;", "duplicate column name");
+            updateVersionTable(c, 54);
         }
         if (db_version < 56) {
-            Statement s2 = c.createStatement();
-            s2.execute("ALTER TABLE orders ADD deleteAfterTimestamp NUMERIC;");
-            s2.close();
+            execIgnore(c, "ALTER TABLE orders ADD deleteAfterTimestamp NUMERIC;", "duplicate column name");
+            updateVersionTable(c, 56);
         }
         if (db_version < 57) {
-            Statement s3 = c.createStatement();
-            s3.execute("CREATE INDEX orderposition_order ON orderposition (order_ref);");
-            s3.close();
-            Statement s4 = c.createStatement();
-            s4.execute("CREATE INDEX checkin_position ON CheckIn (position);");
-            s4.close();
+            execIgnore(c, "CREATE INDEX orderposition_order ON orderposition (order_ref);", "already exists");
+            execIgnore(c, "CREATE INDEX checkin_position ON CheckIn (position);", "already exists");
+            updateVersionTable(c, 57);
         }
         if (db_version < 58) {
-            Statement s3 = c.createStatement();
-            s3.execute("ALTER TABLE CheckIn ADD listId INT;");
-            s3.close();
-            Statement s5 = c.createStatement();
-            s5.execute("UPDATE CheckIn SET listId = list;");
-            s5.close();
-            Statement s4 = c.createStatement();
-            s4.execute("CREATE INDEX checkin_listid ON CheckIn (listId);");
-            s4.close();
+            execIgnore(c, "ALTER TABLE CheckIn ADD listId INT;", "duplicate column name");
+            execIgnore(c, "UPDATE CheckIn SET listId = list WHERE (listID = 0 OR listId IS NULL);", "no such column: list");
+            execIgnore(c, "CREATE INDEX checkin_listid ON CheckIn (listId);", "already exists");
+            updateVersionTable(c, 58);
         }
         if (db_version < 59) {
-            Statement s3 = c.createStatement();
-            s3.execute("ALTER TABLE ResourceLastModified ADD \"meta\" TEXT;");
-            s3.close();
+            execIgnore(c, "ALTER TABLE ResourceLastModified ADD \"meta\" TEXT;", "duplicate column name");
+            updateVersionTable(c, 59);
         }
         if (db_version < 60) {
-            Statement s3 = c.createStatement();
-            s3.execute("ALTER TABLE CheckIn ADD server_id NUMERIC NULLABLE;");
-            s3.close();
-            Statement s4 = c.createStatement();
-            s4.execute("CREATE INDEX checkin_server_id ON CheckIn (server_id);");
-            s4.close();
+            execIgnore(c, "ALTER TABLE CheckIn ADD server_id NUMERIC NULLABLE;", "duplicate column name");
+            execIgnore(c, "CREATE INDEX checkin_server_id ON CheckIn (server_id);", "already exists");
+            updateVersionTable(c, 60);
         }
 
         // Note that the Android app currently does not use these queries!
 
         if (db_version < CURRENT_VERSION) {
-            Statement s4 = c.createStatement();
-            s4.execute("DELETE FROM ResourceLastModified;");
-            s4.close();
+            exec(c, "DELETE FROM ResourceLastModified;");
         }
         updateVersionTable(c, CURRENT_VERSION);
     }
