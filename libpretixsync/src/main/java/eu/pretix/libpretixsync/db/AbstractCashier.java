7989035344f64f496c693265b6babb1e46e444b8
Raphael Michel <mail@raphaelmichel.de>
2021-05-12 16:18:44 +0200

    Fix default var

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/db/AbstractCashier.java
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/db/AbstractCashier.java
@@ -23,7 +23,7 @@ public class AbstractCashier implements RemoteObject, CashierLike {
 
     public String pin;
 
-    @Column(value = "0", nullable = false)
+    @Column(value = "false", nullable = false)
     public boolean active;
 
     @Column(definition = "TEXT")
