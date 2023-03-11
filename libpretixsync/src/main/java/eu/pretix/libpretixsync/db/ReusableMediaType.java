4dcb9a3270c3c4bbcf4abac52ff9ef2a89c1f8ba
Raphael Michel <michel@rami.io>
2023-03-11 16:45:01 +0100

    Add server names of media types

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/db/ReusableMediaType.java
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/db/ReusableMediaType.java
@@ -1,8 +1,14 @@
 package eu.pretix.libpretixsync.db;
 
 public enum ReusableMediaType {
-    NONE,
-    BARCODE,
-    NFC_UID,
-    UNSUPPORTED,
+    NONE(null),
+    BARCODE("barcode"),
+    NFC_UID("nfc_uid"),
+    UNSUPPORTED(null);
+
+    public final String serverName;
+
+    private ReusableMediaType(String serverName) {
+        this.serverName = serverName;
+    }
 }
