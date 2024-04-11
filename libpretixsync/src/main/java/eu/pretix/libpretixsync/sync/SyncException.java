67a3a2722a24492657050f276cfe66177e09334e
Raphael Michel <michel@rami.io>
2024-04-11 13:34:29 +0200

    More detailed exceptions for events with no access (#41)

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/sync/SyncException.java
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/sync/SyncException.java
@@ -4,4 +4,8 @@ public class SyncException extends Exception {
     public SyncException(String message) {
         super(message);
     }
+
+    public SyncException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
