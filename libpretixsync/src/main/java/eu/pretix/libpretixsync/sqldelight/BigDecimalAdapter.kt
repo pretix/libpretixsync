1641d251edbeb857b6ac6b34eb63894e6bb94312
Anton Weber <git@anonweb.de>
2024-06-10 11:39:29 +0200

    Change to BigDecimal.valueOf() in BigDecimalAdapter
    Uses the string representation of the double to create a BigDecimal. The
    previously used constructor on the other hand used the exact
    representation, which - to quote the docs - "can be somewhat
    unpredictable".
    This behaviour is also closer to requery's handling of decimal values.
    
    See https://docs.oracle.com/javase/7/docs/api/java/math/BigDecimal.html#BigDecimal(double)

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/sqldelight/BigDecimalAdapter.kt
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/sqldelight/BigDecimalAdapter.kt
@@ -6,7 +6,7 @@ import java.math.RoundingMode
 
 class BigDecimalAdapter : ColumnAdapter<BigDecimal, Double> {
     override fun decode(databaseValue: Double): BigDecimal {
-        return BigDecimal(databaseValue).setScale(2, RoundingMode.HALF_UP)
+        return BigDecimal.valueOf(databaseValue).setScale(2, RoundingMode.HALF_UP)
     }
 
     override fun encode(value: BigDecimal): Double {
