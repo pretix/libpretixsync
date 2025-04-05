06fc8343244420c3d31442831012799bafdceae9
Raphael Michel <michel@rami.io>
2025-04-05 16:33:14 +0200

    Add a helper function

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/models/Discount.kt
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/models/Discount.kt
@@ -34,4 +34,64 @@ data class Discount(
         SAME,
         DISTINCT
     }
+
+    companion object {
+        /**
+         * only for use in test, not in production code, since the defaults might change server-side
+         * and we should not rely on them
+         */
+        fun withDefaults(
+            id: Long = 1L,
+            serverId: Long = 1L,
+            eventSlug: String = "democon",
+            active: Boolean = true,
+            position: Long = 1L,
+            allSalesChannels: Boolean = true,
+            limitSalesChannels: List<String> = emptyList(),
+            availableFrom: OffsetDateTime? = null,
+            availableUntil: OffsetDateTime? = null,
+            subeventMode: DiscountSubeventMode = DiscountSubeventMode.MIXED,
+            subeventDateFrom: OffsetDateTime? = null,
+            subeventDateUntil: OffsetDateTime? = null,
+            conditionAllProducts: Boolean = true,
+            conditionLimitProducts: List<Long> = emptyList(),
+            conditionApplyToAddons: Boolean = true,
+            conditionIgnoreVoucherDiscounted: Boolean = false,
+            conditionMinCount: Int = 0,
+            conditionMinValue: BigDecimal = BigDecimal("0.00"),
+            benefitSameProducts: Boolean = true,
+            benefitLimitProducts: List<Long> = emptyList(),
+            benefitDiscountMatchingPercent: BigDecimal = BigDecimal("0.00"),
+            benefitOnlyApplyToCheapestNMatches: Int? = null,
+            benefitApplyToAddons: Boolean = true,
+            benefitIgnoreVoucherDiscounted: Boolean = false,
+        ): Discount {
+            return Discount(
+                id = id,
+                serverId = serverId,
+                eventSlug = eventSlug,
+                active = active,
+                position = position,
+                allSalesChannels = allSalesChannels,
+                limitSalesChannels = limitSalesChannels,
+                availableFrom = availableFrom,
+                availableUntil = availableUntil,
+                subeventMode = subeventMode,
+                subeventDateFrom = subeventDateFrom,
+                subeventDateUntil = subeventDateUntil,
+                conditionAllProducts = conditionAllProducts,
+                conditionLimitProducts = conditionLimitProducts,
+                conditionApplyToAddons = conditionApplyToAddons,
+                conditionIgnoreVoucherDiscounted = conditionIgnoreVoucherDiscounted,
+                conditionMinCount = conditionMinCount,
+                conditionMinValue = conditionMinValue,
+                benefitSameProducts = benefitSameProducts,
+                benefitLimitProducts = benefitLimitProducts,
+                benefitDiscountMatchingPercent = benefitDiscountMatchingPercent,
+                benefitOnlyApplyToCheapestNMatches = benefitOnlyApplyToCheapestNMatches,
+                benefitApplyToAddons = benefitApplyToAddons,
+                benefitIgnoreVoucherDiscounted = benefitIgnoreVoucherDiscounted
+            )
+        }
+    }
 }
