96e9ab71ef42f5b9a607dca823250e833e43da97
Anton Weber <git@anonweb.de>
2025-04-16 18:00:50 +0200

    Add constant for the default database name
    Some places use the generated requery value Models.DEFAULT.name to
    determine the name of the database. Since that is going away, we need
    another place to keep that constant.

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/sqldelight/Migrations.kt
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/sqldelight/Migrations.kt
@@ -9,6 +9,13 @@ object Migrations {
     const val MIN_SUPPORTED_VERSION = 77L
 
     /**
+     * Database name
+     *
+     * Corresponds to requery's Models.DEFAULT.name
+     */
+    const val DEFAULT_DATABASE_NAME = "default"
+
+    /**
      * AfterVersion callback that can be used in SQLDelight code migrations to crash if we encounter
      * a schema version that we have no migrations for.
      * Without it, SQLDelight might just increase the DB version without actually creating the
