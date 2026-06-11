d5a6723c5fd3fc759b2af458a3da9ab8df7af651
Maximilian Richt <richt@pretix.eu>
2026-06-11 10:04:00 +0200

    Add MediaPolicy.APPEND and MediaPolicy.APPEND_OR_NEW

--- libpretixsync/src/main/java/eu/pretix/libpretixsync/db/MediaPolicy.kt
+++ libpretixsync/src/main/java/eu/pretix/libpretixsync/db/MediaPolicy.kt
@@ -2,9 +2,11 @@ package eu.pretix.libpretixsync.db
 
 enum class MediaPolicy(val serverName: String?) {
     NONE(null),
-    REUSE("reuse"),
     NEW("new"),
-    REUSE_OR_NEW("reuse_or_new");
+    REUSE("reuse"),
+    REUSE_OR_NEW("reuse_or_new"),
+    APPEND("append"),
+    APPEND_OR_NEW("append_or_new");
 
     companion object {
         private val map = entries.associateBy(MediaPolicy::serverName)
